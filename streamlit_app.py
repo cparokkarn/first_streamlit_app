78a6a378d43c7578d830c0f8815330ff36a169a5
cparokkaran <110378376+cparokkarn@users.noreply.github.com>
2022-08-02 10:06:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My new diner test')
 streamlit.header('Breakfast Menu');
-streamit.text('Omega 3 and blueberry oatmeal');
+streamlit.text('Omega 3 and blueberry oatmeal');
