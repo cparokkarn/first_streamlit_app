52cf4881901a960c6e91387569a2b51a9b32c582
cparokkaran <110378376+cparokkarn@users.noreply.github.com>
2022-08-02 10:07:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
 streamlit.title('My new diner test')
-streamlit.header('Breakfast Menu');
-streamlit.text('Omega 3 and blueberry oatmeal');
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
